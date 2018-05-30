6106f5884c321d8f12d82595bf7d7c607e693830
Pandeya, Gyanesh <Gyanesh.Pandeya@McKesson.com>
2018-05-30 12:55:20 -0700

    moved EnableBatchProcessing to Main file

--- src/main/java/com/mck/p2p/batchjob/debitmemo/debitmemobatch/DebitmemoBatchApplication.java
+++ src/main/java/com/mck/p2p/batchjob/debitmemo/debitmemobatch/DebitmemoBatchApplication.java
@@ -1,9 +1,11 @@
 package com.mck.p2p.batchjob.debitmemo.debitmemobatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class DebitmemoBatchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/mck/p2p/batchjob/debitmemo/debitmemobatch/config/JobConfiguration.java
+++ src/main/java/com/mck/p2p/batchjob/debitmemo/debitmemobatch/config/JobConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableBatchProcessing
+
 public class JobConfiguration {
 
 	@Autowired
