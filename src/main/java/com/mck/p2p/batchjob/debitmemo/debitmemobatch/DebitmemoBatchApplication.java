9599c8a3bfae2099adde2656edf9ddb497a36734
Pandeya, Gyanesh <Gyanesh.Pandeya@McKesson.com>
2018-05-30 12:13:59 -0700

    first commit

---
+++ src/main/java/com/mck/p2p/batchjob/debitmemo/debitmemobatch/DebitmemoBatchApplication.java
@@ -0,0 +1,12 @@
+package com.mck.p2p.batchjob.debitmemo.debitmemobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitmemoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitmemoBatchApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mck/p2p/batchjob/debitmemo/debitmemobatch/config/JobConfiguration.java
@@ -0,0 +1,45 @@
+package com.mck.p2p.batchjob.debitmemo.debitmemobatch.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.JobFactory;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class JobConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepFactory;
+	
+	
+	@Bean
+	public Step step1(){
+		return stepFactory.get("step1").tasklet(new Tasklet(){
+				@Override
+				public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+					System.out.println("Hello World from Debit Memo batch JOB!");
+					return RepeatStatus.FINISHED;
+				}
+			}).build();
+	
+	}
+	
+	@Bean
+	public Job debitMemoBatchJob(){
+		return jobFactory.get("debit-memo-batch").start(step1()).build();
+	}
+	
+}
